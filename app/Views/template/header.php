744405a03c0cf466fa0f0359f05644330c847810
CristianZavala <0701308b@umich.mx>
2022-11-06 07:28:32 -0600

    Se agrega el proyecto

---
+++ app/Views/template/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codeigniter 4 CRUD (Create, Read, Update and Delete) with Bootstrap and Datatables</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+  <style>
+    .error {
+      display: block;
+      padding-top: 5px;
+      font-size: 14px;
+      color: red;
+    }
+  </style>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ app/Views/user_view.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Librería</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-4">
+    <p><h1>Inventario</h1></p>
+    <div class="d-flex justify-content-end">
+        <a href="<?php echo site_url('/user-form') ?>" class="btn btn-success mb-2">Añadir libro</a>
+    </div>
+    <?php
+     if(isset($_SESSION['msg'])){
+        echo $_SESSION['msg'];
+      }
+     ?>
+    <table class="table table-bordered table-striped" id="books-list">
+       <thead>
+          <tr>
+             <th>Id</th>
+             <th>Nombre del libro</th>
+             <th>Autor</th>
+             <th>Acción</th>
+          </tr>
+       </thead>
+       <tbody>
+          <?php if($users): ?>
+          <?php foreach($users as $user): ?>
+          <tr>
+             <td><?php echo $user['id']; ?></td>
+             <td><?php echo $user['name']; ?></td>
+             <td><?php echo $user['email']; ?></td>
+             <td>
+              <a href="<?php echo base_url('edit-view/'.$user['id']);?>" class="btn btn-primary btn-sm">Editar</a>
+              <a href="<?php echo base_url('delete/'.$user['id']);?>" class="btn btn-danger btn-sm">Borrar</a>
+              </td>
+          </tr>
+         <?php endforeach; ?>
+         <?php endif; ?>
+       </tbody>
+    </table>
+</div>
+  
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+      $('#books-list').DataTable();
+  } );
+</script>
+</body>
+</html>
